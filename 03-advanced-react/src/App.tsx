7d87ba900385ea83caa656c1e46d60ffef170a6e
manases24 <hugo.orlando2050@gmail.com>
2024-12-04 13:32:43 -0300

    advanced react

---
+++ 03-advanced-react/src/App.tsx
@@ -0,0 +1,9 @@
+import { UseStateObject } from "./tutorial/01-useState/04-useState-object";
+
+export function App() {
+  return (
+    <>
+      <UseStateObject/>
+    </>
+  );
+}
---
+++ 03-advanced-react/src/api.ts
@@ -0,0 +1,41 @@
+export interface DataType {
+  id: number;
+  name: string;
+}
+
+export interface ImageType {
+  small: {
+    url: string;
+  };
+}
+
+export interface PersonType {
+  id: number;
+  name: string;
+  nickName?: string;
+  images?: ImageType[];
+}
+
+export const data: DataType[] = [
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+  { id: 3, name: "susan" },
+  { id: 4, name: "anna" },
+];
+
+export const people: PersonType[] = [
+  { id: 1, name: "bob", nickName: "Stud Muffin" },
+  { id: 2, name: "peter" },
+  {
+    id: 3,
+    name: "oliver",
+    images: [
+      {
+        small: {
+          url: "https://res.cloudinary.com/diqqf3eq2/image/upload/ar_1:1,bo_5px_solid_rgb:ff0000,c_fill,g_auto,r_max,w_1000/v1595959121/person-1_aufeoq.jpg",
+        },
+      },
+    ],
+  },
+  { id: 4, name: "david" },
+];
