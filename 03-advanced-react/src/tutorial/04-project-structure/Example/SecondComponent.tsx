446bf47c7d2b5e7a30ce487766be26f68921dfd4
manases24 <hugo.orlando2050@gmail.com>
2024-12-05 12:19:05 -0300

    project structure

---
+++ 03-advanced-react/src/tutorial/04-project-structure/Example/SecondComponent.tsx
@@ -0,0 +1,3 @@
+export const SecondComponent = () => {
+  return <div>SecondComponent</div>;
+};
---
+++ 03-advanced-react/src/tutorial/04-project-structure/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import "./Navbar.css"
+
+export function Navbar() {
+  return (
+    <div className="navbar">
+      <h2>navbar component</h2>
+    </div>
+  );
+}
