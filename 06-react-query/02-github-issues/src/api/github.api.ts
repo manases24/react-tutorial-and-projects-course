d5b5c7aa22f055114347c51861c00788052716e6
manases24 <hugo.orlando2050@gmail.com>
2024-12-12 11:21:53 -0300

    github issues - list issues

--- 06-react-query/02-github-issues/src/api/github.api.ts
+++ 06-react-query/02-github-issues/src/api/github.api.ts
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react/",
-  headers: {},
+  // baseURL: "https://api.github.com/repos/vuejs/core/",
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+  },
 });
