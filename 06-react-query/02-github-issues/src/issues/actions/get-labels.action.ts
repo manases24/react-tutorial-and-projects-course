81bc610c3be1a3c214c4434e7d9199258178636a
manases24 <hugo.orlando2050@gmail.com>
2024-12-12 09:53:20 -0300

    github issues app

--- 06-react-query/02-github-issues/src/issues/actions/get-labels.action.ts
+++ 06-react-query/02-github-issues/src/issues/actions/get-labels.action.ts
@@ -1,7 +1,9 @@
 import { githubApi } from "../../api/github.api";
+import { sleep } from "../../helpers/sleep";
 import { GithubLabel } from "../interfaces/label.interface";
 
 export const getLabels = async (): Promise<GithubLabel[]> => {
+  await sleep(1500);
   const { data } = await githubApi.get<GithubLabel[]>("/labels");
   return data;
 };
--- 06-react-query/02-github-issues/src/issues/components/LabelPicker.tsx
+++ 06-react-query/02-github-issues/src/issues/components/LabelPicker.tsx
@@ -4,23 +4,25 @@ import { useLabels } from "../hooks/useLabels";
 export const LabelPicker = () => {
   const { labelsQuery } = useLabels();
 
-  if (labelsQuery.isFetching) {
-    return <LoadingSpinner />;
+  if (labelsQuery.isLoading) {
+    return (
+      <div className="flex justify-center items-center h-52">
+        <LoadingSpinner />
+      </div>
+    );
   }
 
   return (
     <div className="flex flex-wrap gap-2 justify-center">
-      {labelsQuery.data?.map((label) => {
-        return (
-          <span
-            className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white"
-            style={{ border: `1px solid #${label.color}` }}
-            key={label.id}
-          >
-            {label.name}
-          </span>
-        );
-      })}
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white"
+          style={{ border: `1px solid #${label.color}` }}
+        >
+          {label.name}
+        </span>
+      ))}
     </div>
   );
 };
