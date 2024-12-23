665b58535ea903b997fc5d62e0afbac4a7295eeb
manases24 <hugo.orlando2050@gmail.com>
2024-12-23 09:51:44 -0300

    react query - store

--- 06-react-query/03-react-query-store/src/products/services/actions.ts
+++ 06-react-query/03-react-query-store/src/products/services/actions.ts
@@ -29,3 +29,20 @@ export const getProductById = async (id: number): Promise<Product> => {
   const { data } = await productsApi.get<Product>(`/products/${id}`);
   return data;
 };
+
+export interface ProductLike {
+  id?: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+export const createProduct = async (product: ProductLike) => {
+  await sleep(5);
+
+  // throw new Error('Error creando un producto');
+
+  const { data } = await productsApi.post<Product>(`/products`, product);
+  return data;
+};
