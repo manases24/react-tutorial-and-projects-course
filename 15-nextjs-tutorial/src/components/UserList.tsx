daa02846da52bcfaf97ffaa9c124dab34596f3bf
manases24 <hugo.orlando2050@gmail.com>
2025-01-15 10:47:40 -0300

    server actions

--- 15-nextjs-tutorial/src/components/UserList.tsx
+++ 15-nextjs-tutorial/src/components/UserList.tsx
@@ -1,5 +1,20 @@
-export const UserList = () => {
+import { fetchUsers } from "@/utils/actions";
+
+export const UserList = async () => {
+  const users = await fetchUsers();
   return (
-    <div className="text-xl">UserList</div>
-  )
-}
+    <div className="mt-4">
+      {users.length ? (
+        <div>
+          {users.map((user) => (
+            <h4 key={user.id} className="capitalize text-lg">
+              {user.firstName} {user.lastName}
+            </h4>
+          ))}
+        </div>
+      ) : (
+        <p>No users found...</p>
+      )}
+    </div>
+  );
+};
