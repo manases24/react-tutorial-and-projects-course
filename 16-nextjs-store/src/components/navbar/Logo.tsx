084b34cc2fce8269e362423fa8b233373c0437c8
manases24 <hugo.orlando2050@gmail.com>
2025-01-15 17:06:41 -0300

    next-store - navbar

--- 16-nextjs-store/src/components/navbar/Logo.tsx
+++ 16-nextjs-store/src/components/navbar/Logo.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import { DiYahooSmall } from "react-icons/di";
+import { DiJsBadge } from "react-icons/di";
 import { Button } from "../ui/button";
 
 export const Logo = () => {
   return (
     <Button size="icon" asChild>
       <Link href="/">
-        <DiYahooSmall className="w-6 h-6" />
+        <DiJsBadge  className="w-6 h-6" />
       </Link>
     </Button>
   );
